d2c415b01ba63be6be608dcd41f89f1398961071
JackStrick <jonstrickland96@gmail.com>
2022-09-20 21:33:17 -0400

    Hidden Key

--- config.py
+++ config.py
@@ -1,5 +1,5 @@
 config = {
-    'key': 'fe7817d5ab98acfd05492e8ca7e2dc4e',
+    'key': '',
     'geoURL': 'http://api.openweathermap.org/geo/1.0/direct?q={}&limit=1&appid={}',
     'mainURL': 'https://api.openweathermap.org/data/2.5/weather?lat={}&lon={}&appid={}&units=imperial'
 }
